488c5e790fd142e2ff97172997c5ef8a0b36adee
Murciegalo <39123379+Murciegalo@users.noreply.github.com>
2020-09-10 21:18:09 +0100

    getting location when user init App set

--- App.js
+++ App.js
@@ -1,9 +1,30 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, {useEffect,useState} from 'react';
+import { StyleSheet, View , Text} from 'react-native';
+import * as Location from 'expo-location'
+
 
 export default function App() {
+  const [error, setError] = useState(null)
+  useEffect(() => load(), [])
+
+  async function load(){
+    try {
+      let { status } = await Location.requestPermissionsAsync()
+      if(status !== 'granted') {
+        setError('Application need you location please')
+        return;
+      }
+      const location = await Location.getCurrentPositionAsync()
+      const { latitude , longitude } = location.coords
+      alert(latitude , longitude)
+    } 
+    catch (error) {
+      console.log(error)  
+    }
+  }
   return (
     <View style={styles.container}>
+      <Text>TEST</Text>
     </View>
   );
 }
