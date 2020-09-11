63d55890f3a928418bb689562389aae1b7ff843e
Murciegalo <39123379+Murciegalo@users.noreply.github.com>
2020-09-11 14:44:00 +0100

    api call set , lat & long set

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import React, {useEffect,useState} from 'react';
 import { StyleSheet, View , Text} from 'react-native';
-import * as Location from 'expo-location'
-
+import * as Location from 'expo-location';
 
 export default function App() {
   const [error, setError] = useState(null)
+  const [weather, setWeather] = useState(null)
   useEffect(() => load(), [])
 
   async function load(){
@@ -16,15 +16,36 @@ export default function App() {
       }
       const location = await Location.getCurrentPositionAsync()
       const { latitude , longitude } = location.coords
-      alert(latitude , longitude)
+
+      const weather=`${Expo.Constants.manifest.extra.baseUrl}lat=${latitude}&lon=${longitude}&appid=${Expo.Constants.manifest.extra.apiKey}`
+      const res = await fetch(weather)
+      const result = await res.json()
+
+      if(res.ok) {
+        setWeather(result);
+      }
+      else {
+        setError(result.message)
+      }
     } 
     catch (error) {
       console.log(error)  
     }
   }
+  
+  if (weather) {
+    const {
+      main: { temp },
+    } = weather;
+    return (
+      <View style={styles.container}>
+        <Text>{temp}</Text>
+      </View>
+    );
+  }
   return (
     <View style={styles.container}>
-      <Text>TEST</Text>
+      <Text>{error}</Text>
     </View>
   );
 }
