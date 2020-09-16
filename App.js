f121d44125113cffbc5d61f173e0c47d969a9bb4
Murciegalo <39123379+Murciegalo@users.noreply.github.com>
2020-09-16 15:11:24 +0100

    logic for reset_Btn completed

--- App.js
+++ App.js
@@ -11,44 +11,44 @@ export default function App() {
   const [error, setError] = useState(null)
   const [weather, setWeather] = useState(null)
   const [unitSystem, setUnitSystem] = useState('metric')
-
   useEffect(() => {
-    async function load() {
-      setWeather(null);
-      setError(null);
-      try {
-        let { status } = await Location.requestPermissionsAsync();
-        if (status !== "granted") {
-          setError("Application need you location please");
-          return;
-        }
-        const location = await Location.getCurrentPositionAsync();
-        const { latitude, longitude } = location.coords;
-
-        const weather = `${Expo.Constants.manifest.extra.baseUrl}lat=${latitude}&lon=${longitude}&units=${unitSystem}&appid=${Expo.Constants.manifest.extra.apiKey}`;
-        const res = await fetch(weather);
-        const result = await res.json();
-
-        if (res.ok) {
-          setWeather(result);
-        } else {
-          setError(result.message);
-        }
-      } catch (error) {
-        console.log(error);
-      }
-    }
     load()
   }
   ,[unitSystem])
 
+  async function load() {
+    setWeather(null);
+    setError(null);
+    try {
+      let { status } = await Location.requestPermissionsAsync();
+      if (status !== "granted") {
+        setError("Application need you location please");
+        return;
+      }
+      const location = await Location.getCurrentPositionAsync();
+      const { latitude, longitude } = location.coords;
+
+      const weather = `${Expo.Constants.manifest.extra.baseUrl}lat=${latitude}&lon=${longitude}&units=${unitSystem}&appid=${Expo.Constants.manifest.extra.apiKey}`;
+      const res = await fetch(weather);
+      const result = await res.json();
+
+      if (res.ok) {
+        setWeather(result);
+      } else {
+        setError(result.message);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  
   if (weather) {
     return (
       <View style={styles.container}>
         <StatusBar style="auto" />
         <View style={styles.main}>
           <UnitsPicker  unitSystem={unitSystem} setUnitSystem={setUnitSystem}/>
-          <ReloadIcon />
+          <ReloadIcon load={load} />
           <WeatherInfo weather={weather}/>
         </View>
       </View>
--- components/ReloadIcon.js
+++ components/ReloadIcon.js
@@ -3,11 +3,11 @@ import { View, Platform , StyleSheet } from 'react-native'
 import {Ionicons} from '@expo/vector-icons'
 import {colors} from '../utils'
 
-export default function ReloadIcon() {
+export default function ReloadIcon({load}) {
   const reloadIconName = Platform.OS === 'ios' ? 'ios-refresh' : 'md-refresh' 
   return (
     <View style={styles.reloadIcon}>
-      <Ionicons name={reloadIconName} size={24} color={colors.PRIMARY_COLOR} />
+      <Ionicons onPress={load} name={reloadIconName} size={24} color={colors.PRIMARY_COLOR} />
     </View>
   )
 }
